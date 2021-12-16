1db9d2835afcca30e817dc8172713ef11c9f4cde
Tom Hofmann <tom@scheidweg.net>
2021-12-16 14:17:42 +0100

    fix remote loading

--- static/ttt_remote.js
+++ static/ttt_remote.js
@@ -13,8 +13,11 @@ class TttView {
         
         for (const button of this.grid.getElementsByTagName("button")) {
             const cellText = json.field[i];
+            // Set the data-state attribute which drives CSS formatting.
             button.setAttribute('data-state', cellText);
+            // Set the text contents of the cell.
             button.textContent = cellText;
+            // Set the disabled state to match whether the cell is full.
             if (cellText != ' ') {
                 button.setAttribute('disabled', "1");
             } else {
@@ -29,7 +32,6 @@ class TttView {
 class TttController {
     constructor(view) {
         this.view = view;
-
         this.init();
     }
 
@@ -37,38 +39,52 @@ class TttController {
      * Fetches the game state, updates the UI, and installs click handlers.
      */
     async init() {
-        try {
-            let response = await fetch(`/join`);
-            let json = await response.json();
-            this.player = json.player;
-            this.next = json.next;
-            this.game_id = json.id;
-            this.view.updateHtml(json);
-        } catch (exception) {
-            console.log(exception);
-        }
+        this.handleJsonUrl('/join');
 
         let i = 0;
         for (const button of this.view.grid.getElementsByTagName("button")) {
             const cell = i;
             button.addEventListener('click', () => {
-                this.onClick(cell);
+                this.handleJsonUrl(`/set/${this.game_id}/${this.player}/${cell}`);
             });
             i++;
         }
     }
 
-    async onClick(cell) {
+    async fetchCurrentGame() {
+        this.handleJsonUrl(`/view/${this.game_id}/${this.player}`);
+    }
+
+    async handleJsonUrl(url) {
         try {
-            let response = await fetch(`/set/${this.game_id}/${this.player}/${cell}`);
-            let json = await response.json();
-            this.next = json.next;
-            this.view.updateHtml(json)
+            let response = await fetch(url);
+            await this.handleJsonResponse(response);
+            if (this.state == "WAITING" || (this.state == "PLAYING" && this.next != this.player)) {
+                await this.wait(1000);
+                this.fetchCurrentGame();
+            }
         } catch (exception) {
             console.log(exception);
         }
     }
 
+    async handleJsonResponse(response) {
+        if (!response.ok) {
+            console.log("Error: " + await response.text());
+            return;
+        }
+        const json = await response.json();
+        this.player = json.player;
+        this.next = json.next;
+        this.game_id = json.id;
+        this.state = json.state;
+        this.view.updateHtml(json);
+    }
+
+    // wait ms milliseconds
+    wait(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 }
 
 let view = new TttView(document.getElementsByClassName("grid")[0])
