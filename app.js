d6187a5f87b641008304ae79c66b72e5a6d01186
Tom Hofmann <tom@scheidweg.net>
2024-08-09 12:27:09 +0200

    add redirect and comments

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+// Serve all files from static/ as is.
+// For example, a request for '/tictactoe.html' will be served from
+// 'static/tictactoe.html'
 app.use(express.static('static'))
 
+// Redirect the root url to tictactoe.
+app.get('/', (req, res) => {
+    res.redirect('tictactoe.html')
+})
+
+// Listen on the given port
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
